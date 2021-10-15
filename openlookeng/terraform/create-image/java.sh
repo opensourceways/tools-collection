cdee8f205968d88b61965412decd28ce78e9ebb2
adnbj <78532744+adnbj@users.noreply.github.com>
2021-10-15 16:10:47 +0800

    Update java.sh

--- openlookeng/terraform/create-image/java.sh
+++ openlookeng/terraform/create-image/java.sh
@@ -2,7 +2,7 @@
 
 yum --showduplicate list java*
 
-#yum install -y java-1.8.0-openjdk-headless-1.8.0.282.b08-1.el7_9
+
 yum install -y java-1.8.0-openjdk-1.8.0.282.b08-1.el7_9.x86_64
 yum install -y java-1.8.0-openjdk-devel-1.8.0.282.b08-1.el7_9.x86_64
 
